4d145bfecbb5767ee58e0a02f57f91c81d74de21
Riya-Vaishya8 <riyavaishya17@gmail.com>
2025-12-23 17:17:00 +0530

    Updates

---
+++ _next/static/qNoGvNHTE6r_qGjWOLjme/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/markdown-portfolio-generator//_next/:path+",
+        "destination": "/markdown-portfolio-generator/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
